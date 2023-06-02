b6924381d7c9bd6e360848de3e2c7b87099f7af0
d13g0-rar-dev <diego2210063@correo.uis.edu.co>
2023-06-02 16:07:40 -0500

    pagina 1 completa

--- main.js
+++ main.js
@@ -1,18 +1,74 @@
+var iter = 0;
 
-  $(document).ready(function() {
-    $('#btnAgregar').click(function() {
-      var nombre = $('#nombre').val();
-      var apellido = $('#apellido').val();
-      
-      // Creamos una nueva fila con los datos ingresados
-      var fila = '<tr><td>' + nombre + '</td><td>' + apellido + '</td></tr>';
-      
-      // Agregamos la nueva fila al cuerpo de la tabla
-      $('#miTabla tbody').append(fila);
-      
-      // Limpiamos los campos de entrada
-      $('#nombre').val('');
-      $('#apellido').val('');
+var group1;
+
+function addRows() {
+  iter += 1;
+  group1 = document.getElementById("gust0").value;
+  const tbody = document.querySelector("tbody");
+  tbody.innerHTML +=
+    '<tr>\
+                            <th scope="row">' +
+    group1 +
+    '\
+                            <td>\
+                                0\
+                            </td>\
+                            <td href="#" onclick="editRows()">\
+                                <div id="edit' +
+    iter +
+    '">\
+                                    <a href="#">\
+                                        Editar\
+                                    </a>\
+                                </div>\
+                            </td>\
+                      </tr>';
+
+  console.log(tbody);
+}
+
+function editRows() {
+  alert("Please doble click to edit");
+
+  document.querySelectorAll("a").forEach((cell) => {
+    cell.addEventListener("click", (evt) => {
+      console.log("The element that was clicked was ", evt.target);
+
+      var editBtun = evt.target;
+      var editTr = editBtun.parentNode.parentNode.parentNode;
+
+      console.log(editTr);
+
+      editTr.innerHTML =
+        '<th scope="row">\
+                                 <input class="form" id="gusto" value="' +
+        group1 +
+        '">\
+                                </input>\
+                            </th>\
+                            <td>\
+                                <input class="form" id="porcent" value="0">\
+                                </input>\
+                            </td>\
+                            <td>\
+                                <div>\
+                                        Editando\
+                                </div>\
+                            </td>';
+
+      var divBut = document.getElementById("finalButton");
+
+      divBut.innerHTML =
+        '<button id="cancelar" type="button" onclick="cancelFun()">Cancelar</button><button id="aceptar" type="button" onclick="acceptFun()">Aceptar</button>';
     });
   });
+}
 
+function acceptFun() {
+  window.location.href = "index2.html";
+}
+
+function cancelFun() {
+  location.reload();
+}
